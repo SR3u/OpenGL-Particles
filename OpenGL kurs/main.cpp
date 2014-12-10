400dcd23cfdb59cf9b04fb5a012287f59fbd9f15
Sergey Rump <wnmsr3u@gmail.com>
2014-12-10 15:45:24 +0300

    Initial Commit

---
+++ OpenGL kurs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL kurs
+//
+//  Created by SR3u on 10.12.14.
+//  Copyright (c) 2014 SR3u. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
